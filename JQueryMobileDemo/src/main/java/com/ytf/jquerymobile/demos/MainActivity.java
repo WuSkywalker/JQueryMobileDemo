5050f68c076d7a21798ffc299c356205d12373ef
Skywalker <andyhua2014@gmail.com>
2014-12-23 15:13:12 +0800

    update

--- JQueryMobileDemo/src/main/java/com/ytf/jquerymobile/demos/MainActivity.java
+++ JQueryMobileDemo/src/main/java/com/ytf/jquerymobile/demos/MainActivity.java
@@ -2,6 +2,7 @@ package com.ytf.jquerymobile.demos;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.KeyEvent;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
@@ -50,5 +51,14 @@ public class MainActivity extends BaseActivity {
         mWebView.loadUrl("file:///android_asset/index.html");
     }
 
+    // 如果不做任何处理，浏览网页，点击系统“Back”键，整个Browser会调用finish()而结束自身，
+    // 如果希望浏览的网 页回退而不是推出浏览器，需要在当前Activity中处理并消费掉该Back事件。
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {
+            mWebView.goBack();
+            return true;
+        }
+        return false;
+    }
 
 }
